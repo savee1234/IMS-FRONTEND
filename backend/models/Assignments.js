1f2ff45ffc4450e689c67c84704f501c9b10f551
raveesha gimhan <146311393+R3aveesha@users.noreply.github.com>
2025-11-27 09:19:35 +0530

    update

--- backend/models/Assignments.js
+++ backend/models/Assignments.js
@@ -1,14 +1,22 @@
 const mongoose = require("mongoose");
+
 const assignmentSchema = new mongoose.Schema({
-  complaintId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Complaint",
+  assignedTo: {
+    type:mongoose.Schema.Types.ObjectId,
+    ref: 'Usermanagement',
     required: true,
   },
-  assignedTo: { type: String, required: true },
-  assignedBy: { type: String, required: true },
-  status: { type: String, enum: ["assigned", "in-progress", "completed"], default: "assigned" },
-  remarks: String,
-}, { timestamps: true });
+  assignedBy: {
+    type: String,
+    required: true,
+  },
+  Assignment:{
+    type:String,
+    enum:['Main Assignment','Sub Assignment'],
+    required:true
+  }
 
-module.exports = mongoose.model("Assignment", assignmentSchema);
+});
+
+module.exports = mongoose.model("Assignments", assignmentSchema);  
+  

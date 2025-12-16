7e3b90e0d9a9050a6f795e951673aea92811c4d6
raveesha gimhan <146311393+R3aveesha@users.noreply.github.com>
2025-12-16 10:46:25 +0530

    update

--- backend/models/Assignments.js
+++ backend/models/Assignments.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const assignmentSchema = new mongoose.Schema({
-  assignedTo: {
+  assignedTo: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Usermanagement',
     required: true
-  },
+  }],
   assignedBy: {
     type: String,
     required: true
